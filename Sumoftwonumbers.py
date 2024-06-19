72ed1dc9f852cc2ea07397d582abbf4659147653
Prasthuthi-593 <prasthuthi593@gmail.com>
2024-06-19 20:07:11 +0530

    first file

---
+++ Sumoftwonumbers.py
@@ -0,0 +1,9 @@
+# Input: two numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Calculate the sum
+sum_result = num1 + num2
+
+# Output the result
+print("The sum of {} and {} is {}".format(num1, num2, sum_result))
